85a948053a1d82bc285516cd2f232fa216d1d860
dbulkowski95 <d.bulkowski95@gmail.com>
2020-04-01 22:38:26 +0200

    Init commit with my basic set-up script

---
+++ README.md
@@ -0,0 +1,22 @@
+# Set-up and Dotfiles
+
+I decide to create set-up script with some dotfiles for fast start on new linux environment. 
+Here are my dotfiles config, just to have them in one place.
+
+## Installation
+
+### Prerequisits
+- [Vim](https://www.vim.org/)
+
+Instaling from scrip, using `prepare.sh`, download the file beafore the repo:
+```bash
+chmod +x set_up.sh
+./set_up.sh
+```
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ set_up.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+## Update packages and Upgrade system
+sudo apt-get update - y > /dev/null 2>&1
+
+## Git ##
+echo '### Installing Git..'
+sudo apt-get install git -y > /dev/null 2>&1
+
+# Git Configuration
+echo '### Congigure Git..'
+
+echo "Enter the Global Username for Git:";
+read GITUSER;
+git config --global user.name "${GITUSER}"
+
+echo "Enter the Global Email for Git:";
+read GITEMAIL;
+git config --global user.email "${GITEMAIL}"
+
+echo 'Git has been configured!'
+git config --list
+
+## Vim ##
+echo '### Installing Vim..'
+sudo apt-get install vim -y > /dev/null 2>&1
+
+## Set vim to default editor ##
+echo 'Set vim to default editor'
+[ -n $EDITOR ] && EDITOR=vim
+
+## Tree ##
+echo '### Installing Tree..'
+sudo apt-get install tree -y > /dev/null 2>&1
+
+## Python ##
+echo '### Installing python..'
+sudo apt-get install python -y > /dev/null 2>&1
+
+## Upgrade packages ##
+echo '### Upgrade packages..'
+sudo apt-get upgrade -y > /dev/null 2>&1
+
+echo 'Finished'
